40d9341322b6bbed24ff05d715faa0bc71c372e6
matheus costa <dev.matheuspc@gmail.com>
2026-02-05 13:01:07 -0300

    alterado base da url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [tailwindcss()],
   // Garante que os links funcionem no GitHub Pages
-  base: '/leandro/',
+  base: '/2eng_solucoes/',
 });
